68fa2a135fd08565412da2a80cd8eb4949e65ef2
Ronald Libago <code@ronaldlibago.com>
2019-03-05 11:27:44 -0800

    Add supporting information for minesweeper

--- CodeSignal/minesweeper.js
+++ CodeSignal/minesweeper.js
@@ -4,6 +4,8 @@ function minesweeper(matrix) {
   // to all adjacent positions in the copy. Return the copy.
   // Helper function
   const incrementAdjacentsInGrid = (x, y) => {
+    // Mutates "grid" by incrementing all valid adjacent
+    // positions around x,y in "grid"
     /*
      * [x - 1, y - 1], [x, y - 1], [x + 1, y - 1]
      * [x - 1, y],     [x, y],     [x + 1, y]
@@ -44,3 +46,35 @@ function minesweeper(matrix) {
 matrix = [[true, false, false], [false, true, false], [false, false, false]];
 
 console.log(minesweeper(matrix));
+
+/* In the popular Minesweeper game you have a board with some mines and those cells that don't contain a mine have a number in it that indicates the total number of mines in the neighboring cells. Starting off with some arrangement of mines we want to create a Minesweeper game setup.
+
+Example
+
+For
+
+matrix = [[true, false, false],
+          [false, true, false],
+          [false, false, false]]
+
+the output should be
+
+minesweeper(matrix) = [[1, 2, 1],
+                       [2, 1, 1],
+                       [1, 1, 1]]
+
+Input/Output
+
+    [execution time limit] 4 seconds (js)
+
+    [input] array.array.boolean matrix
+
+    A non-empty rectangular matrix consisting of boolean values - true if the corresponding cell contains a mine, false otherwise.
+
+    Guaranteed constraints:
+    2 ≤ matrix.length ≤ 100,
+    2 ≤ matrix[0].length ≤ 100.
+
+    [output] array.array.integer
+        Rectangular matrix of the same size as matrix each cell of which contains an integer equal to the number of mines in the neighboring cells. Two cells are called neighboring if they share at least one corner.
+*/
