dcea8675f51d28d771f0c1f5b7e9b1251f78ab0a
Ronald Libago <code@ronaldlibago.com>
2019-03-06 15:35:32 -0800

    Complete circleOfNumbers

---
+++ CodeSignal/through-the-fog/circleOfNumbers.js
@@ -0,0 +1,34 @@
+function circleOfNumbers(n, firstNumber) {
+  const posFrom0 = num => Math.floor(num / 2);
+  const radialPos = (num, size, firstNum) => (num + firstNum) % size;
+  return radialPos(posFrom0(n), n, firstNumber);
+}
+
+/*
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+Example
+
+For n = 10 and firstNumber = 2, the output should be
+circleOfNumbers(n, firstNumber) = 7.
+
+Input/Output
+
+    [execution time limit] 4 seconds (js)
+
+    [input] integer n
+
+    A positive even integer.
+
+    Guaranteed constraints:
+    4 ≤ n ≤ 20.
+
+    [input] integer firstNumber
+
+    Guaranteed constraints:
+    0 ≤ firstNumber ≤ n - 1.
+
+    [output] integer
+*/
