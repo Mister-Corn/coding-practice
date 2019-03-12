8a26cc77fe40c81d4cdc6842b4698de08115730e
Ronald Libago <code@ronaldlibago.com>
2019-03-12 16:04:32 -0700

    Complete solution for balancedBrackets in Lambda repo

---
+++ Lambda-Whiteboard-Pairing/BalancedBrackets/mySolution.js
@@ -0,0 +1,39 @@
+function balancedBrackets(str) {
+  /* We'll use a stack to solve this problem.
+   * We'll iterate through the string. Whenever we come across a left
+   * bracket, we'll put it's corresponding right bracket in the stack.
+   * When we encounter a right bracket, we'll pop the stack and compare
+   * the popped element with the current one.
+   * If they're different, it's false
+   * If there's anything left on the stack after the loop, it's false
+   * Otherwise, it's true
+   */
+  let stack = [];
+  const leftBrackets = ["[", "{", "("];
+  const rightBrackets = ["]", "}", ")"];
+
+  for (const char of str) {
+    if (leftBrackets.includes(char)) {
+      // Push corresponding right bracket to stack
+      const index = leftBrackets.indexOf(char);
+      stack.push(rightBrackets[index]);
+    } else if (rightBrackets.includes(char)) {
+      // Pop stack and compare popped element with char
+      const poppedElem = stack.pop();
+      if (char !== poppedElem) {
+        return false;
+      }
+    }
+  }
+
+  // Return true if the stack is empty
+  return stack.length === 0;
+}
+
+// Tests
+// Below should return true
+balancedBrackets("[]{}()"); //?
+// Below should return true
+balancedBrackets("[{[()]}]"); //?
+// Below should return false
+balancedBrackets("[({}}]"); //?
